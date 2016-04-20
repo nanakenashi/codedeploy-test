181d8de54871683b07dbe042d575572d4ccdba4a
nanakenashi <nanakenashi@gmail.com>
2016-04-20 16:10:59 +0000

    Test shell script

--- scripts/success.sh
+++ scripts/success.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-touch ./success.txt
+sed "s/%MYSQL_USER_ID%/$MYSQL_USER_ID/g" /usr/local/etc/codedeploy-test/src/test.html
+sed "s/%MYSQL_PASSWORD%/$MYSQL_PASSWORD/g" /usr/local/etc/codedeploy-test/src/test.html
