aa8006b711e7d97aa84d5e7811cd35a789cface0
nanakenashi <nanakenashi@gmail.com>
2016-04-20 23:17:04 +0000

    Modified

--- scripts/success.sh
+++ scripts/success.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+env > /usr/local/etc/codedeploy-test/src/env.txt
 sed -i "s/%MYSQL_USER_ID%/$MYSQL_USER_ID/g" /usr/local/etc/codedeploy-test/src/test.html
 sed -i "s/%MYSQL_PASSWORD%/$MYSQL_PASSWORD/g" /usr/local/etc/codedeploy-test/src/test.html
