7f00749656c5143dffee115f423c4737ee527f89
nanakenashi <nanakenashi@gmail.com>
2016-04-20 16:28:52 +0000

    Modified

--- scripts/success.sh
+++ scripts/success.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i "s/%MYSQL_USER_ID%/$MYSQL_USER_ID/g" /usr/local/etc/codedeploy-test/src/test.html
+sed -i "s/%MYSQL_USER_ID%/$MAIL/g" /usr/local/etc/codedeploy-test/src/test.html
 sed -i "s/%MYSQL_PASSWORD%/$MYSQL_PASSWORD/g" /usr/local/etc/codedeploy-test/src/test.html
